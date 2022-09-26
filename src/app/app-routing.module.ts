8b6423b53b8250841454bc2393f663431725f22e
@magdiellinares <magdiellinares@gmail.com>
2022-09-26 18:30:35 -0400

    Clase 4: Angular Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MedicComponent } from './pages/medic/medic.component';
+import { PatientEditComponent } from './pages/patient/patient-edit/patient-edit.component';
 import { PatientComponent } from './pages/patient/patient.component';
 
 const routes: Routes = [
-  { path: 'pages/patient', component: PatientComponent },
+  { 
+    path: 'pages/patient', component: PatientComponent, children: [
+      { path: 'new', component: PatientEditComponent},
+      { path: 'edit/:id', component: PatientEditComponent}
+    ]  
+  },
   { path: 'pages/medic', component: MedicComponent }
 ];
 
